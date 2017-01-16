756516809fe5395c0ffc7086c9b5a6d7d0dc6321
Herbert Hart <hart_herbert@hotmail.com>
2017-01-16 14:06:55 +0100

    added JPEG

--- src/android/Canvas2ImagePlugin.java
+++ src/android/Canvas2ImagePlugin.java
@@ -120,10 +120,10 @@ public class Canvas2ImagePlugin extends CordovaPlugin {
 				folder = Environment.getExternalStorageDirectory();
 			}
 
-			File imageFile = new File(folder, "c2i_" + date.toString() + ".png");
+			File imageFile = new File(folder, "IMG-Anigram-" + date.toString() + ".jpg");
 
 			FileOutputStream out = new FileOutputStream(imageFile);
-			bmp.compress(Bitmap.CompressFormat.PNG, 100, out);
+			bmp.compress(Bitmap.CompressFormat.JPEG, 100, out);
 			out.flush();
 			out.close();
 
